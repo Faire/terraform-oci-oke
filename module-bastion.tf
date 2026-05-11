fcec7ef50352dc5551c5fb20030c2c05db619ab3
Kaelen Proctor <kaelen.proctor@faire.com>
2026-05-11 14:42:23 -0400

    Support passing additional userdata through for bastion nodes

--- module-bastion.tf
+++ module-bastion.tf
@@ -62,6 +62,7 @@ module "bastion" {
   upgrade                        = var.bastion_upgrade
   user                           = var.bastion_user
   volume_kms_key_id              = var.bastion_volume_kms_key_id
+  cloud_init                     = var.bastion_cloud_init
 
   # Standard tags as defined if enabled for use, or freeform
   # User-provided tags are merged last and take precedence
